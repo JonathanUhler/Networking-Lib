de67f2bf4597cd24cf3ca0bd26014f06475ef571
Jonathan Uhler <jonathan@uhler.com>
2024-07-27 17:54:09 -0700

    increase RANDOM_TEST_AMOUNT for jnet tests

--- tests/jnet/TestMain.java
+++ tests/jnet/TestMain.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class TestMain {
 
-    static final int RANDOM_TEST_AMOUNT = 10000;
+    static final int RANDOM_TEST_AMOUNT = 100000;
     static final Random RANDOM_DIS = new Random();
 
 
